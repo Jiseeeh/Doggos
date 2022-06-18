7be19706f1cee072bbbe2d54de20a8a6cc0ef87a
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-19 04:27:46 +0800

    Add validation

--- js/dogPictureFetcher.js
+++ js/dogPictureFetcher.js
@@ -1,7 +1,13 @@
-function fetchDogImg() {
+function fetchDogImg(value) {
   const imgContainer = document.querySelector(".content__images");
-  let value = "borzoi";
-  for (let i = 0; i < 10; i++) {
+  const numberOfImg = prompt("How many images do you want? ");
+
+  if (parseInt(numberOfImg) > 100 || parseInt(numberOfImg) <= 0) {
+    prompt("Please input properly.");
+    return;
+  }
+
+  for (let i = 0; i < parseInt(numberOfImg); i++) {
     fetch(`https://dog.ceo/api/breed/${value}/images/random`)
       .then((response) => response.text())
       .then((data) => {
