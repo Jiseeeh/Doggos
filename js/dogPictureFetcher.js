6cc5a95a589ba9d29d40b93425ffbe81c34ef842
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-19 18:14:24 +0800

    Remove prompt and set range to fixed

--- js/dogPictureFetcher.js
+++ js/dogPictureFetcher.js
@@ -1,13 +1,8 @@
 function fetchDogImg(value) {
   const imgContainer = document.querySelector(".content__images");
-  const numberOfImg = prompt("How many images do you want? ");
+  imgContainer.innerHTML = ""
 
-  if (parseInt(numberOfImg) > 100 || parseInt(numberOfImg) <= 0) {
-    prompt("Please input properly.");
-    return;
-  }
-
-  for (let i = 0; i < parseInt(numberOfImg); i++) {
+  for (let i = 0; i < 10; i++) {
     fetch(`https://dog.ceo/api/breed/${value}/images/random`)
       .then((response) => response.text())
       .then((data) => {
